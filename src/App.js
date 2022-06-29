a517c9120013976c2d9db2f4a95963e872509498
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-30 02:15:19 +0300

    UseRefExample1 - create DOM Reference

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import UseRefExample1 from "./components/UseRefExample1";
+
 
 function App() {
   return (
     <div className='container mt-5'>
-      <h1>hello</h1>
+      <UseRefExample1/>
     </div>
   );
 }
