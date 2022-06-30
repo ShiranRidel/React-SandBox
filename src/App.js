631705c6ddf48e6ec4b244bd31dbf1f7ce7f9c24
ShiranRidel <86361159+ShiranRidel@users.noreply.github.com>
2022-06-30 21:42:08 +0300

    UseMemo Example

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import UseMemoExample from "./components/UseMemoExample";
 import UseRefExample1 from "./components/UseRefExample1";
 import UseRefExample2 from "./components/UseRefExample2";
 import UseRefExample3 from "./components/UseRefExample3";
@@ -6,8 +7,12 @@ import UseRefExample3 from "./components/UseRefExample3";
 function App() {
   return (
     <div className='container mt-5'>
+      <UseMemoExample/>
+      ----------------------------------------------------------------------------------------
       <UseRefExample3/>
+      ----------------------------------------------------------------------------------------
       <UseRefExample2/>
+      ----------------------------------------------------------------------------------------
       <UseRefExample1/>
 
     </div>
